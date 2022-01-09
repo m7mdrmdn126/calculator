7edce704239b2ff4a6d9e992819d8fff11f6a344
mohamed ramadan <mohamed.ramdan@ejsut.edu.eg>
2022-01-09 14:21:05 -0500

    prj

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    result : 0 ,
+    num1 : '',
+    num2 : '',
+    opp : ''
   },
   mutations: {
+    res(state) {
+      if(state.opp === '+'){
+        state.result = parseInt(state.num1) + parseInt(state.num2)
+      }
+      else if(state.opp === '-'){
+        state.result = parseInt(state.num1) - parseInt(state.num2)
+      }
+      else if(state.opp === '*'){
+        state.result = parseInt(state.num1) * parseInt(state.num2)
+      }
+      else if(state.opp === '/'){
+        state.result = parseInt(state.num1) / parseInt(state.num2)
+      }
+      
+    }
   },
   actions: {
+    
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="grey lighten-5">
+         <!-- output -->
+
+        <v-row
+          class="mt-6 "
+        >
+        <p class="dispaly-4 mx-auto"> {{ $store.state.result }}</p>
+        
+        </v-row>
+
+        <!--inputs -->  
+        <v-row
+           style="height: 150px;"
+        >
+        <v-text-field
+          v-model="$store.state.num1"
+          label="first number "
+          class="ma-5"
+          type="number"
+          
+        > 
+        </v-text-field>
+
+
+      <v-text-field
+          v-model="$store.state.opp"
+          label="opreator "
+          class="ma-5"
+          
+        > 
+        </v-text-field>
+
+
+        <v-text-field
+          v-model="$store.state.num2"
+          label="second number "
+          class="ma-5"
+          type="number"
+        > 
+        </v-text-field>
+        </v-row>
+
+        <v-row>
+          <v-btn
+            @click="$store.commit('res')"
+            color="primary"
+            elevation="7"
+            block
+            plain
+            small
+            text
+            class="mx-auto"
+          >
+            calculate
+          </v-btn>
+        </v-row>
+      </v-container>
+
+ 
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    data: () => ({
+    alignments: [
+      'start',
+      'center',
+      'end',
+    ],
+  }),
   }
-}
 </script>
